42e0e2da12482e14cc769cde4cd2c5065f40b1cb
k3v1n12 <ambalikevin@gmail.com>
2023-07-30 19:14:01 +0100

    creating the scene

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //Create a scene
+    QGraphicsScene* scene = new QGraphicsScene();
+
+    //Create an item to add to the scene
+    QGraphicsRectItem* rectItem  = new QGraphicsRectItem();
+
+    //configure item
+    rectItem->setRect(0, 0, 100, 100);
+
+    //add item to scene
+    scene->addItem(rectItem);
+
+    //create a view to visualise the scene
+    QGraphicsView* view = new QGraphicsView(scene);
+
+    //show view
+    view->show();
 
     return a.exec();
 }
